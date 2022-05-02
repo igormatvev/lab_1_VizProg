5baefc37e6ed5b4530f821d710a911242726644c
igormatvev <78473335+igormatvev@users.noreply.github.com>
2022-05-02 14:11:56 +0700

    Add files via upload

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1
+{
+    public class HW1
+    {
+        public static long QueueTime(int[] customers, int n)
+        {
+            int[] cashiers = new int[n];
+            int cashier = 0;
+            int point = 0;
+
+            for (int j = 0; j < n; j++)
+            {
+                cashiers[j] = customers[j];
+            }
+            point = n - 1;
+
+            while (true)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    cashiers[j] -= 1;
+                    cashier += 1;
+                    if (cashiers[j] == 0)
+                    {
+                        if (point >= customers.Length - 1 && cashiers.Max() == 0)
+                        {
+                            while (cashier % n != 0)
+                            {
+                                cashier += 1;
+                            }
+                            Console.WriteLine("Clients = {0}", string.Join(", ", customers));
+                            Console.WriteLine("Sellers = {0}", n);
+                            Console.WriteLine("Result = {0}\n", (cashier / n));
+                            return 0;
+                        }
+                        else if (point < customers.Length - 1)
+                        {
+                            point += 1;
+                            cashiers[j] = customers[point];
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lab_1
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            HW1.QueueTime(new int[] { 5, 3, 4 }, 1);
+            HW1.QueueTime(new int[] { 10, 2, 3, 3 }, 2);
+            HW1.QueueTime(new int[] { 2, 3, 10 }, 2);
+        }
+    }
+}
